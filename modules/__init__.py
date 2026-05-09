ec82ee7f2c40b494298483a20aa4aafd49f29a3e
alikkiplik <alikkiplik@gmail.com>
2026-05-09 12:39:21 +0300

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,11 @@
+from .text_editor import TextEditorModule
+from .paint import PaintModule
+from .media_player import MediaPlayerModule
+from .settings import SettingsModule
+
+__all__ = [
+    "TextEditorModule",
+    "PaintModule",
+    "MediaPlayerModule",
+    "SettingsModule"
+]
